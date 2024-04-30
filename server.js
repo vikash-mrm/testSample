db7bf44cd45c5283fe1c806dd7f1f038a66c86fb
Vikash <vikash@mrmprocom.com>
2024-04-30 16:28:32 +0530

    2nd

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 // Import required modules
+require('dotenv').config()
 const express = require('express');
 
 // Create an instance of Express
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 // Define a route to handle GET requests to the root URL
 app.get('/', (req, res) => {
-  res.send('Hello, World!'); // Respond with "Hello, World!"
+  res.send('Hello, Duniya!'); // Respond with "Hello, World!"
 });
 
 // Start the server and listen on the specified port
